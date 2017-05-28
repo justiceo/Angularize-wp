5960530e743040cf4bb0ea2cf760a4fa835da3f1
Justice Ogbonna <ebesco@yahoo.com>
2017-05-28 15:07:47 -0400

    minor cleanup

--- src/app/app.controller.js
+++ src/app/app.controller.js
@@ -2,9 +2,6 @@ export default class AppCtrl {
   constructor(Cache, PostService) {
     'ngInject';
     Cache.set("test", "testValue");
-    console.log(Cache.get('test'));
-    console.log(PostService.the_ID());    
     this.sample = 'angular works!';
-	console.log(PostService.get_post());
   }
 }
