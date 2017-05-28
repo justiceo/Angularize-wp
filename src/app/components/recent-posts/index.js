68b77f80213793209b0cb324508ba75bac985c86
Justice Ogbonna <ebesco@yahoo.com>
2017-05-28 15:16:27 -0400

    updated recent post with link

--- src/app/components/recent-posts/index.js
+++ src/app/components/recent-posts/index.js
@@ -1,7 +1,6 @@
-//import RecentPosts from './recent-posts';
-export class RecentPosts {
+ export class RecentPosts {
     constructor(PostService) {
-        PostService.get_posts().then(
+        PostService.get_posts().then(            
             posts => this.posts = posts.slice(0,5)            
         );
     }
@@ -9,7 +8,16 @@ export class RecentPosts {
 
 let RecentPostsComponent = {
     controller: RecentPosts,
-    template: require('./recent-posts.html')
+    template: `
+    <h2>Recent Posts</h2>
+    <ul>
+        <li ng-repeat="post in $ctrl.posts">
+            <a href="{{ post.link }}">
+                {{ post.title.rendered }}
+            </a>
+        </li>
+    </ul>
+    `
 }
 
 export default RecentPostsComponent;
