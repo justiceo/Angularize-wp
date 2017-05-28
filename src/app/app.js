f7c1c4f5f246f4d38bce1cac23e9b4f0afb45fe5
Justice Ogbonna <ebesco@yahoo.com>
2017-05-28 16:47:42 -0400

    added component test

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,5 @@
 export class AppCtrl {
   constructor(Cache, PostService) {
-    'ngInject';
-    Cache.set("test", "testValue");
     this.sample = 'angular works!';
   }
 }
--- src/app/app.spec.js
+++ src/app/app.spec.js
@@ -1,22 +1,18 @@
+import app from './index';
 
-// re-write tests from https://puigcerber.com/2016/02/07/how-to-test-angular-1-5-components/
-import app from './app';
+describe('AppComponent', () => {
+  let element;
 
-describe('app', () => {
+  beforeEach(() => {
+    angular.mock.module(app);
 
-  describe('AppCtrl', () => {
-    let ctrl;
+    angular.mock.inject(($rootScope, $compile) => {
+      element = $compile("<app></app>")($rootScope)
+      $rootScope.$digest();
+    })      
+  });
 
-    beforeEach(() => {
-      angular.mock.module(app);
-
-      angular.mock.inject(($controller) => {
-        ctrl = $controller('AppCtrl', {});
-      });
-    });
-
-    it('should contain the sample text', () => {
-      expect(ctrl.sample).toBe('angular works!');
-    });
+  it('should contain the sample text', () => {
+    expect(element.html()).toContain('angular works!');
   });
 });
