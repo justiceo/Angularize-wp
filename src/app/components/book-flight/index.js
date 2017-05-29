bdfff72062f86693dbc20687dcdc99577d22918b
Justice Ogbonna <ebesco@yahoo.com>
2017-05-29 06:42:38 -0400

    book-flight template updates

--- src/app/components/book-flight/index.js
+++ src/app/components/book-flight/index.js
@@ -1,6 +1,14 @@
 export class BookFlightCtrl {
-    constructor($log) {
+    constructor($log, $window) {
         $log.info("BookFlight: Initializing...");
+        angular.extend(this, {'$window': $window, '$log': $log})
+    }
+
+    fly() {
+        if(!this.from || !this.to) return; // we need both fields
+        console.log("flying from " + this.from + " to " + this.to);
+        let win = this.$window.open('https://google.com', '_blank');   
+        win.focus();
     }
 }
 
