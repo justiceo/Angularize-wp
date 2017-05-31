79dbdd23e004cbfb88ca56e46166680b610268a1
Justice Ogbonna <ebesco@yahoo.com>
2017-05-31 11:39:51 -0400

    install angular material

--- src/app/editor/index.js
+++ src/app/editor/index.js
@@ -1,10 +1,17 @@
-
+import ngMaterial from 'angular-material';
 import PostSettings from './settings';
 
 let MODULE_NAME = "AngularizeEditor";
 
 angular.module(MODULE_NAME, [
-    ])
+    ngMaterial,
+  ])
+  .config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+     .primaryPalette('purple')
+     .accentPalette('indigo')
+  })
+
     .component('postSettings', PostSettings);
 
 export default MODULE_NAME;
