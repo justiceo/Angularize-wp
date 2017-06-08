ddbd90ca3c150b4be3867a0e74b4fc20e4dbc0d5
Justice Ogbonna <ebesco@yahoo.com>
2017-06-08 12:33:20 -0400

    still on this schema thing

--- src/app/providers/schema.js
+++ src/app/providers/schema.js
@@ -3,37 +3,87 @@ var _ = require("underscore");
 
 class RestCollection {
 
-    constructor(endpoint, Schema) {
+    constructor(endpoint, parent, Schema) {
+        this._schema = Schema;
         this._value = [];
-        this._endpoint = endpoint;
-        this._modelRef = new RestObject(this._endpoint + '/' + Schema.getModel(endpoint), Schema);
+        this._parent = parent;
+        this._route = this._parent + '/' + endpoint;
+        this._modelRef = new RestObject(Schema.getModel(this._route), this._route,  Schema);
+
+        // process args
     }
 
-    at(index) { return this._value.splice(index, 1); }
-    id(postId) { return this._value.filter(p => p.id == postId); }
+    at(index) { 
+        // fetch object at index
+        let value = this._value.splice(index, 1); // check bounds
+        return new RestObject(value.id, this._route, this._schema, value);
+    }
+
+    id(postId) {
+        let postArr = this._value.filter(p => p.id == postId);
+        let value = postArr.size() == 0 ? null: postArr[0]
+        return new RestObject(postId, this._route, this.Schema, value);
+    }
 
     val() { return this._value }
-    get(args) { } // for getting posts
-    post(payload) { } // for adding new data of type _modelRef;    
+
+    valOrGet() {
+        if(this._value)
+            return this._value;
+        this.get();
+        return this._value;
+    }
+
+    get(args) {
+        // process args and append to route
+        return this.Ajax.get(this._route).then(
+            success => {
+                success.data.forEach(e => this._value.push(this.toRestObj(e)));
+                return this._value;
+            }
+        )
+     } 
+
+     toRestObj(e) {
+         return new RestObject(); // complete here
+     }
+
+    post(payload) { 
+        return this.Ajax.post(this._route, payload).then(
+            success => {
+                return success.data;
+            }
+        )
+    }    
 }
 
 class RestObject {
-    constructor(endpoint, Schema) {
-        this._endpoint = endpoint;
-        this._args = {}; // the lite collection
-        this._args.deep = {}; // the complete collection.
+    constructor(endpoint, parent, Schema, model) {
+        this._parent = parent;
+        this._route = this._parent + '/' + endpoint;
+        this._model = model;
 
         // get the args for the different methods and append them
+        let args = Schema.getArgs(this._route);
+        this._args = this.trimArgs(args);
+        this._args.deep = args;
 
         // add collections that extend from there
-        Schema.getCollections(endpoint).forEach(e => {
-            this[e] = new RestCollection(this._endpoint + '/' + e, Schema);
+        if(Schema) {
+        Schema.getCollections(this._route).forEach(e => {
+            this[e] = new RestCollection(e, this._route, Schema);
         });
+        }
+    }
+
+    trimArgs(args) {
+        // todo: non-critical
+        return args;
     }
 
     $(route) {
         if (!this[route])
-            throw this._endpoint + '/' + route + " - route not reachable from model"
+            throw this._route + '/' + route + " - route not reachable from model"
         return this[route];
     }
 
@@ -73,6 +123,11 @@ class Schema {
             }
         }
         this.routes = Object.keys(this.schema.routes).map(r => r.replace("parent", "id"));
+    }
+
+    getArgs(endpoint) {
+        // todo: non-critical
+        return {};
     }
 
     getModel(endpoint) {
