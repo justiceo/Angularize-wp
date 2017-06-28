48d9e47cd921b930f0311ce906dc019f93c90686
Justice Ogbonna <ebesco@yahoo.com>
2017-06-28 14:03:27 -0400

    cleaned up uploader component

--- src/app/uploader/upload-file.js
+++ src/app/uploader/upload-file.js
@@ -1,48 +1,46 @@
 
 export class UploadFileCtrl {
-    constructor($mdDialog, Upload) {
+    constructor(Upload) {
         this.Upload = Upload;
-        this.$mdDialog = $mdDialog;        
     }
 
     upload(file, alt_text = "", caption = "") {
-        console.log("upload file called");
-            let mediaUrl = window.location.origin + '/wp-json/wp/v2/media'
-            this.Upload.upload({
-                url: mediaUrl,
-                method: 'POST',
-                file: file,
-                headers: {
-                    "Content-Disposition": 'attachment; filename=' + file.name,
-                    "Content-Type": file.type,
-                    "Cache-Control": "no-cache",
-                    "Data-Binary": file.name
-                },
-                data: {
-                    'caption': caption,
-                    'alt_text': alt_text
-                }
-            })
+        let mediaUrl = window.location.origin + '/wp-json/wp/v2/media'
+        this.Upload.upload({
+            url: mediaUrl,
+            method: 'POST',
+            file: file,
+            headers: {
+                "Content-Disposition": 'attachment; filename=' + file.name,
+                "Content-Type": file.type,
+                "Cache-Control": "no-cache",
+                "Data-Binary": file.name
+            },
+            data: {
+                'caption': caption,
+                'alt_text': alt_text
+            }
+        })
             .then((resp) => {
                 this.fileUrl = resp.data.source_url;
-                console.log('Success ' + resp.data.title.rendered + ' uploaded.');
-                //this.scope.onUploaded({$uploadLink: this.fileUrl});
+                this.onUploaded({ $uploadLink: this.fileUrl });
             }, function (resp) {
                 console.log('Error uploading file: ' + resp);
             }, (evt) => {
                 this.progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-                console.log('progress: ' + this.progressPercentage + '% ' + evt.config.data.file.name);
-                //this.scope.progress({$uploadPercent: this.progressPercentage});
+                this.progress({ $uploadPercent: this.progressPercentage });
             });
     }
 }
 
-
-
 let UploadFile = {
     controller: UploadFileCtrl,
     template: require('./upload-file.html'),
-    transclude: true
+    transclude: true,
+    bindings: {
+        progress: '&',
+        onUploaded: '&'
+    }
 }
 
 export default UploadFile;
