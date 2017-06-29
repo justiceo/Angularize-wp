ef90bdc89fb74ef3ad5178731fbe9a66a848059d
Justice Ogbonna <ebesco@yahoo.com>
2017-06-29 17:33:08 -0400

    update and track deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-webpack
-scp -P22 ./local/dir username@servername.com:/remote/dir 
+tsc rest-api.ts --outDir ./src/app/providers/
+webpack --production
+cp -r angularize_wp /var/www/html/wp-content/plugins/
+zip -r angularize_wp.zip angularize_wp
