e08105b4bce55cdffb17d0ab6f39a1af32efbaa8
Justice Ogbonna <ebesco@yahoo.com>
2017-07-23 00:47:16 -0400

    add test dependencies. refactored test

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,22 +1,14 @@
 describe('TestComponent', function() {
   var element, scope;
   
-  /*beforeEach(function() {
-    angular.mock.module('angularize');
+  beforeEach(function() {
+    module('angularize');
 
-    angular.mock.inject(function($rootScope, $compile) {
+    inject(function($rootScope, $compile) {
       element = $compile("<test></test>")($rootScope)
-      console.log("element: ", element)
       $rootScope.$digest();
     })      
-  });*/
-
-  beforeEach( module('angularize'))
-
-  beforeEach( inject( function($rootScope, $compile){
-    element = $compile('<test></test>')($rootScope);
-    $rootScope.$digest();
-  }));
+  });
 
   it('should contain the sample text', function() {
     expect(element.html()).toContain('angular works!');
