370ab071c0ad9195ea8a413728374013291145c5
Justice Ogbonna <ebesco@yahoo.com>
2017-07-23 00:52:43 -0400

    stop loading angular more than once

--- karma.conf.js
+++ karma.conf.js
@@ -4,9 +4,8 @@ module.exports = function(config) {
     browsers: ['PhantomJS'],
     frameworks: ['jasmine'],
     files: [
-      'node_modules/angular/angular.min.js',
-      'node_modules/angular-mocks/angular-mocks.js',
       'build/assets/main.js',
+      'node_modules/angular-mocks/angular-mocks.js',
       'test/**/*.spec.js'
     ]
   });
