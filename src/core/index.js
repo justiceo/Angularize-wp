1cb72dcebfbef5d4ce57c188671ea97b3d48bad1
Justice Ogbonna <ebesco@yahoo.com>
2017-07-25 11:56:55 -0400

    some cool console stuff

--- src/core/index.js
+++ src/core/index.js
@@ -8,6 +8,10 @@ let angularizeCore = angular.module('angularizeCore', [])
 
 // comment-out this line to stop printing debug messages in console
 console.debug = console.log;
+// using console.table -- real cool!
+// https://blog.mariusschulz.com/2013/11/13/advanced-javascript-debugging-with-consoletable
+// using colors in console -- another cooly!
+// https://i.stack.imgur.com/DFJBd.png
 
 angularizeCore
   .component('toolbar', Toolbar)
--- src/core/mock-service.js
+++ src/core/mock-service.js
@@ -1,4 +1,7 @@
-
+/**
+ * Resolves failed requests to WordPress application while developing outside WordPress.
+ * [NOTE] This class cannot depened on $http or any class that depends on it as it is an $httpRequestInterceptor
+ */
 export default class MockService {
     constructor($q){
         this.$q = $q;
@@ -7,7 +10,7 @@ export default class MockService {
 
     resolve(rejection) {
         let method = rejection.config.method, url = rejection.config.url;
-        let res = this.endPoints.filter(e => e.method == method && e.url == url)
+        let res = mockResponses.filter(e => e.method == method && e.url == url)
         res = res.length > 0 ? res[0] : null
         if(res){            
             console.warn('MOCK: resolved [' + method + '] ' + url);
@@ -21,9 +24,9 @@ export default class MockService {
     }
 
     mock(method, url, response) {
-        let dup = this.endPoints.filter(e => e.method == method && e.url == url);
+        let dup = mockResponses.filter(e => e.method == method && e.url == url);
         if(dup) console.warn('MOCK: adding duplicate mocks for [' + method + '] ' + url);
-        this.endPoints.push({method: method, url:url, response: response});
+        mockResponses.push({method: method, url:url, response: response});
     }
 
     init() {
@@ -36,25 +39,42 @@ export default class MockService {
                 postObject: {},
             }
 
-        this.endPoints = [
-            {
-                'method': 'GET',
-                'url': 'http://localhost:3000/wp-json/wp/v2',
-                'response': {
-                    routes: {
-                        '/wp/v2': '',
-                        '/wp/v2/posts':'',
-                        '/wp/v2/posts/(?P<id>[\d]+)': '',
-                        '/wp/v2/users':'',
-                        '/wp/v2/users/(?P<id>[\d]+)':'',
-                        '/wp/v2/users/me':'',
-                        '/wp/v2/categories':'',
-                        '/wp/v2/categories/(?P<id>[\d]+)':'',
-                        '/wp/v2/tags':'',
-                        '/wp/v2/tags/(?P<id>[\d]+)':'',
-                    }
-                }
-            }
-        ]
+        
     }
 }
+
+var mockRedirects =
+[
+    {
+        'method': 'GET',
+        'url': 'http://localhost:3000/wp-json/angularize/v1',
+        'response': 'http://localhost:3000/files/schema.json'
+    },
+    {
+        'method': 'GET',
+        'url': 'http://localhost:3000/wp-json/wp/v2/posts?per_page=5',
+        'response': 'http://localhost:3000/files/recent-5.json'
+    }
+]
+
+var mockResponses = 
+[
+    {
+        'method': 'GET',
+        'url': 'http://localhost:3000/wp-json/wp/v2',
+        'response': {
+            routes: {
+                '/wp/v2': '',
+                '/wp/v2/posts':'',
+                '/wp/v2/posts/(?P<id>[\d]+)': '',
+                '/wp/v2/users':'',
+                '/wp/v2/users/(?P<id>[\d]+)':'',
+                '/wp/v2/users/me':'',
+                '/wp/v2/categories':'',
+                '/wp/v2/categories/(?P<id>[\d]+)':'',
+                '/wp/v2/tags':'',
+                '/wp/v2/tags/(?P<id>[\d]+)':'',
+            }
+        }
+    }
+]
