da90ac6f730a4f5ea5049c2129ad15a749359750
Justice Ogbonna <ebesco@yahoo.com>
2017-08-03 19:41:37 -0400

    add markup for reaction

--- src/components/reaction.js
+++ src/components/reaction.js
@@ -1,64 +1,36 @@
-/**
- * Like functionality for posts and other content
- * - Allows user to like and unlike posts
- * - Try as much as possible to prevent duplicate likes
- * Usage: <i class="fa fa-heart" like="213"></i>
- * where 213 is post id.
- */
 export class ReactionCtrl {
     constructor(RestApi) {
-        this.restrict = 'A';
-        this.transclude = true;
-        this.scope = {};
         this.RestApi = RestApi;
-        this.likeCount = 0;
-        this.template = function( element, attrs ) {
-            var tag = element[0].nodeName;
-            return "<"+tag+" data-ng-transclude ng-*=''></"+tag+">";
-        };
     }
-
-    compile(element, attr) {
-        // determine if user has liked this post and set the like class
-        element.css('color', 'red');
-        element.addClass("like")
-        let postId = attr.like;
-        /*this.CurrentUser.getMeta('likes').then(
-            likes => {
-                //if(likes.contain(postId)) 
-                    element.addClass('liked')
+    $onInit() {
+        this.buttons = [
+            {
+                id: 'like-button',
+                title: 'like',
+                icon: 'em em---1'
+            },
+            {
+                id: 'joy-button',
+                title: "Crackin' Up",
+                icon: 'em em-joy'
+            },
+            {
+                id: 'soso-button',
+                title: 'so so',
+                icon: 'em em-expressionless'
+            },
+            {
+                id: 'enlightened-button',
+                title: "Learned sometin'",
+                icon: 'em em-bulb'
             }
-        )*/
-        return this.link
-    }
-
-    link(scope, element, attr) {
-        element.bind('click', () => {
-            if(element.hasClass('liked')) {
-                element.removeClass('liked');
-                this.unlike(attr.like);
-                --this.likeCount;
-            }
-            else {
-                element.addClass('liked');
-                this.like(attr.like);
-                ++this.likeCount;
-            }
-        });
-    }
-
-    like(postId) {
-        console.log("liked ", postId);
-    }
-
-    unlike(postId) {
-        console.log("unliked ", postId);
+        ]
     }
 }
 
-let Reaction = function(RestApi) {
-    'ngInject';
-    return new ReactionCtrl(RestApi);
+let Reaction = {
+    controller: ReactionCtrl,
+    templateUrl: 'components/reaction.html'
 }
 
 export default Reaction;
