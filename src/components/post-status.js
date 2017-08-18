717e26324ac1986c1e518e4207de33ba28b84915
Justice Ogbonna <ebesco@yahoo.com>
2017-08-17 22:54:07 -0400

    increase size a bit

--- src/components/post-status.js
+++ src/components/post-status.js
@@ -7,7 +7,7 @@ export class PostStatusCtrl {
 let PostStatus = {
     controller: PostStatusCtrl,
     template: `
-    <div class="btn-group btn-group-sm" role="group" aria-label="Post Status">
+    <div class="btn-group" role="group" aria-label="Post Status">
         <button type="button" ng-click="$ctrl.setActive('draft')" ng-class="{active: $ctrl.status=='draft'}" class="btn btn-default">Draft</button>
         <button type="button" ng-click="$ctrl.setActive('pending')" ng-class="{active: $ctrl.status=='pending'}" class="btn btn-default">Pending Review</button>
         <button type="button" ng-click="$ctrl.setActive('publish')" ng-class="{active: $ctrl.status=='publish'}" class="btn btn-default">Published</button>
