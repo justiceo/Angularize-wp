7b0c08f2105e11bbc2f3df26d84853ac3863bec4
Justice Ogbonna <ebesco@yahoo.com>
2017-08-17 22:52:55 -0400

    fix post status publish instead of published

--- src/components/post-status.js
+++ src/components/post-status.js
@@ -2,10 +2,6 @@ export class PostStatusCtrl {
     setActive(status) {
         this.status = status;
     }
-
-    $onInit() {
-        console.log("ps: ", this.status);
-    }
 }
 
 let PostStatus = {
@@ -14,7 +10,7 @@ let PostStatus = {
     <div class="btn-group btn-group-sm" role="group" aria-label="Post Status">
         <button type="button" ng-click="$ctrl.setActive('draft')" ng-class="{active: $ctrl.status=='draft'}" class="btn btn-default">Draft</button>
         <button type="button" ng-click="$ctrl.setActive('pending')" ng-class="{active: $ctrl.status=='pending'}" class="btn btn-default">Pending Review</button>
-        <button type="button" ng-click="$ctrl.setActive('published')" ng-class="{active: $ctrl.status=='published'}" class="btn btn-default">Published</button>
+        <button type="button" ng-click="$ctrl.setActive('publish')" ng-class="{active: $ctrl.status=='publish'}" class="btn btn-default">Published</button>
     </div>
     `,
     bindings: {
