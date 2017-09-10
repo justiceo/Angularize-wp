df7bd71e70bc1da112c742d9bf02deb9f0352a5f
Justice Ogbonna <ebesco@yahoo.com>
2017-09-10 01:00:28 -0400

    more enhancements to editorial component

--- src/components/editorial.js
+++ src/components/editorial.js
@@ -5,12 +5,14 @@ export class EditorialCtrl {
                 this.pendingPosts = RestApi.$wp_v2.posts({ 'status': 'pending', '_embed': true })
                 this.pendingPosts.get().then(() => {
                     this.pendingPosts.map(p => {
+                        p.featuredImage = "https://www.jainsusa.com/images/store/landscape/not-available.jpg";
                         try {
+                            p.editLink = window.location.origin + "/new-post/?" + p.attr('id');
                             p.authorName = p.state._embedded['author'][0]['name'];
-                            p.featuredImage = p.state._embedded['wp:featuredmedia'][0].source_url;  
-
-                        } catch(e) { 
-                            console.log("unable to set featured image for post: ", p.attr('title'))
+                            p.authorLink = p.state._embedded['author'][0]['link'];
+                            p.featuredImage = p.state._embedded['wp:featuredmedia'][0].source_url; 
+                        } catch(e) {
+                            console.log("unable to set (author or) featured image for post: ", p.attr('title'))
                         }                                               
                         return p;
                     });
